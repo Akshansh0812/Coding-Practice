ae2d5fb17a973b6d77eb0242547272329ac5256a
Akshansh0812 <akshanshanand2001@gmail.com>
2023-02-26 23:45:00 +0530

    Q152-Maximum product subarray

---
+++ src/Leetcode/Q152.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+// https://leetcode.com/problems/maximum-product-subarray/
+public class Q152 {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, product = 1;
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            max = Math.max(product *= nums[i], max);
+            if (nums[i] == 0) product = 1;
+        }
+
+        product = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            max = Math.max(product *= nums[i], max);
+            if (nums[i] == 0) product = 1;
+        }
+        return max;
+    }
+    private void run() {
+        int[] nums = new int[]{2,3,-2,4};
+        maxProduct(nums);
+    }
+    public static void main(String[] args) {
+        new Q152().run();
+    }
+}
