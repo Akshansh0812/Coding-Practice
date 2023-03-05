6fa3f2af21b432d59c2fe2ac28e5528eb1f3c86a
Akshansh0812 <akshanshanand2001@gmail.com>
2023-03-05 15:32:40 +0530

    102-Binary Tree Level Order Traversal

---
+++ src/Leetcode/Q102.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+public class Q102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int level = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i<level; i++){
+                TreeNode temp = q.remove();
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+                list.add(temp.val);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
