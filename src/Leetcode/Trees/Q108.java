63e35ffeb3ac3c26c5a3aa55b428c41e9124f80e
Akshansh0812 <akshanshanand2001@gmail.com>
2023-03-26 03:25:43 +0530

    Array to list

--- src/Leetcode/Trees/Q108.java
+++ src/Leetcode/Trees/Q108.java
@@ -1,5 +1,20 @@
 package Leetcode.Trees;
 
 public class Q108 {
-
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length-1);
+    }
+    private TreeNode helper(int[] nums, int left, int right){
+        if(left > right) {
+            return null;
+        }
+        if(left == right){
+            return new TreeNode(nums[left]);
+        }
+        int mid = (left+right)/2;
+        TreeNode temp = new TreeNode(nums[mid]);
+        temp.left = helper(nums, left, mid-1);
+        temp.right = helper(nums, mid+1, right);
+        return temp;
+    }
 }
