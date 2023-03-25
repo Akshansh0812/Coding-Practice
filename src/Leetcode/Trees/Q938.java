c368fe012636bae59c665d2c4c977591a27afb00
Akshansh0812 <akshanshanand2001@gmail.com>
2023-03-26 03:27:26 +0530

    Range sum of BST

---
+++ src/Leetcode/Trees/Q938.java
@@ -0,0 +1,17 @@
+package Leetcode.Trees;
+
+public class Q938 {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) return 0;
+
+        if(root.val >= low && root.val <= high){
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low ,high);
+        }
+        else if(root.val < low){
+            return rangeSumBST(root.right, low, high);
+        }
+        else{
+            return rangeSumBST(root.left, low, high);
+        }
+    }
+}
